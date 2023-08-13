bac323bc82acfea0338deb40c5452b9a1153a31d
Peter Marks <pmarks@signiant.com>
2023-08-12 20:11:57 -0400

    imagine a fizzbuzz that works

--- c/fizzbuzz.c
+++ c/fizzbuzz.c
@@ -1,12 +1,13 @@
 #include <stdio.h>
-#include <string.h>
 
 int main() {
-  for(int i = 1; i <= 100; i++) {
-   char text[8] = "";
-   text[8] = strcat("", i % 3 == 0 ? "Fizz" : "");
-   text[8] = strcat(text, (i % 5 == 0) ? "Buzz" : "");
-   text[8] = strlen(text) == 0 ? (char)i + "\n" : text;
-   printf("%s\n", text);
+  for(int i=1; i<=100; i++) {
+    if (i % 5 != 0 && i % 3 != 0) {
+      printf("%d", i);
+    } else {
+      if (i % 3 == 0) { printf("Fizz"); }
+      if (i % 5 == 0) { printf("Buzz"); }
+    }
+    printf("\n");
   }
 }
