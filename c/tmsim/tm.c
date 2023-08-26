f320ce8a0c29959a1a4f341f93b27fc0fc6af79c
Peter Marks <peter.marks@gmail.com>
2023-08-25 20:02:07 -0400

    data structure rework in progress

--- c/tmsim/tm.c
+++ c/tmsim/tm.c
@@ -40,37 +40,36 @@ void initialize_tm(uint8_t difficulty, struct time_machine* tm) {
 }
 
 /***** bitwise helper functions *****/
-// TODO: redo these to work with the new bitwise values & state checks
 
-// get bitwise power status of an exterior part by ID
-bool get_exterior_power(uint8_t part, uint8_t byte) {
+// get bitwise power status of a critical part by ID
+bool get_critical_power(uint8_t part, uint8_t byte) {
     switch (part) {
-    case AIRLOCK:   return byte & AIRLOCK_ON;   break;
-    case SHIELD:    return byte & SHIELD_ON;    break;
-    case HOVER:     return byte & HOVER_ON;     break;
-    case TESLA:     return byte & TESLA_ON;     break;
-    case FUSION:    return byte & FUSION_ON;    break;
-    case STEAM:     return byte & STEAM_ON;     break;
-    case IS_READY:  return byte & ALL_READY;    break;
-    case PC_CHECK:  return byte & PC_READY;     break;
+    case POWER:     return byte & POWER_ON;     break;
+    case SUPPORT:   return byte & SUPPORT_ON;   break;
+    case AIRLOCK :  return byte & AIRLOCK_ON;   break;
+    case CIRCUITS:  return byte & CIRCUITS_ON;  break;
+    case CONSOLE:   return byte & CONSOLE_ON;   break;
+    case RC2014:    return byte & RC2014_ON;    break;
+    case CRITICAL:  return byte & CRITICAL_ON;  break;
+    case PWR_LOCK:  return byte & PWR_LOCK_ON;  break;
     default:        return 0;   // do error stuff later
     }
 }
 
-// get bitwise power status of an interior part by ID
-bool get_interior_power(uint8_t part, uint8_t byte) {
+
+// get bitwise power status of an auxillary part by ID
+bool get_auxillary_power(uint8_t part, uint8_t mask) {
     switch (part) {
-    case RC2014:    return byte & RC2014;       break;
-    case POWER:     return byte & SHIELD_ON;    break;
-    case SUPPORT:   return byte & HOVER_ON;     break;
-    case CIRCUITS:  return byte & TESLA_ON;     break;
-    case SENSORS:   return byte & FUSION_ON;    break;
-    case CONSOLE:   return byte & STEAM_ON;     break;
-    case IS_READY:  return byte & ALL_READY;    break;
-    case PC_CHECK:  return byte & PC_READY;     break;
+    case SENSORS:   return mask & SENSORS_ON;   break;
+    case SHIELD:    return mask & SHIELD_ON;    break;
+    case HOVER:     return mask & HOVER_ON;     break;
+    case TESLA:     return mask & TESLA_ON;     break;
+    case FUSION:    return mask & FUSION_ON;    break;
+    case STEAM:     return mask & STEAM_ON;     break;
     default:        return 0;   // do error stuff later
     }
 }
+
 
 void set_bits(uint8_t* byte, uint8_t mask) {
     *byte = *byte | mask;
@@ -186,26 +185,26 @@ void turn_off_part(uint8_t id, uint8_t type, struct time_machine* tm) {
 
 /***** print functions *****/
 
-char* get_exterior_part(uint8_t part) {
+char* get_critical_part(uint8_t part) {
     switch (part) {
-    case 0:     return "Time Machine Airlock";  break;
+    case 0:     return "Main Energy Cells";     break;
+    case 1:     return "Power Distrib. Unit";   break;
+    case 2:     return "Life Support System";   break;
+    case 3:     return "Time Machine Airlock";  break;
+    case 4:     return "Time Travel Circuits";  break;
+    case 5:     return "Time Machine Console";  break;
+    default:    return "Something went wrong";
+    }
+}
+
+char* get_auxillary_part(uint8_t part) {
+    switch (part) {
+    case 0:     return "Threat Sensor System";  break;
     case 1:     return "Insibility Shield";     break;
     case 2:     return "Hover Propulsion";      break;
     case 3:     return "Tesla Charging Coil";   break;
     case 4:     return "Mr Fusion Reactor";     break;
     case 5:     return "Steam Charging Unit";   break;
-    default:    return "Something went wrong";
-    }
-}
-
-char* get_interior_part(uint8_t part) {
-    switch (part) {
-    case 0:     return "Main Energy Cells";     break;
-    case 1:     return "Power Distrib. Unit";   break;
-    case 2:     return "Life Support System";   break;
-    case 3:     return "Time Travel Circuits";  break;
-    case 4:     return "Threat Sensor System";  break;
-    case 5:     return "Time Machine Console";  break;
     default:    return "Something went wrong";
     }
 }
--- c/tmsim/tm.h
+++ c/tmsim/tm.h
@@ -8,13 +8,6 @@
 * TMSim - Time machine header file
 ***************************************/
 
-// basic on/off values
-#define OFF     0   // component off
-#define ON      1   // component on
-
-#define OPEN    0   // air lock open
-#define CLOSED  1   // air lock closed
-
 // display statuses
 #define NOM     0   // nominal status
 #define INF     1   // info status
@@ -22,76 +15,71 @@
 #define FLT     3   // fault status
 
 // part types
-#define EXTERIOR    0
-#define INTERIOR    1
+#define CRITICAL    0
+#define AUXILLARY   1
 #define COMPUTER    2
 
-// exterior part status ids
-#define AIRLOCK     0
+// critical part status ids
+#define POWER       0
+#define SUPPORT     1
+#define AIRLOCK     2
+#define CIRCUITS    3
+#define CONSOLE     4
+#define RC2014      5
+#define CRITICAL    6
+#define PWR_LOCK    7
+
+// auxillary part status ids
+#define SENSORS     0
 #define SHIELD      1
 #define HOVER       2
 #define TESLA       3
 #define FUSION      4
 #define STEAM       5
 
-#define IS_READY    6
-#define PC_CHECK    7
-
-// interior part status ids
-#define RC2014      0
-#define POWER       1
-#define SUPPORT     2
-#define CIRCUITS    3
-#define SENSORS     4
-#define CONSOLE     5
-
 // RC2014 computer part ids
 #define BACKPLANE   0
 #define CLOCK       1
-#define CPU         2
+#define Z80_CPU     2
 #define MEMORY      3
 #define SERIAL_IO   4
 #define HW_BRIDGE   5
 
-// exterior power bitwise vales
-#define AIRLOCK_ON  1
+// critical power bitwise values
+#define POWER_ON    1
+#define SUPPORT_ON  2
+#define CIRCUITS_ON 4
+#define AIRLOCK_ON  8
+#define CONSOLE_ON  16
+#define RC2014_ON   32
+#define CRITICAL_ON 64
+#define PWR_LOCK_ON 128
+
+// critical status bitwise values
+#define POWER_OK    1
+#define SUPPORT_OK  2
+#define AIRLOCK_OK  4
+#define CIRCUTS_OK  8
+#define CONSOLE_OK  16
+#define RC2014_OK   32
+#define CRITICAL_OK 64
+#define COORDS_OK   128
+
+// auxillary power bitwise vales
+#define SENSORS_ON  1
 #define SHIELD_ON   2
 #define HOVER_ON    4
 #define TESLA_ON    8
 #define FUSION_ON   16
 #define STEAM_ON    32
 
-#define AIRLOCK_FLT 64
-#define SHIELD_FLT  128
-#define HOVER_FLT   256
-#define TESLA_FLT   512
-#define FUSION_FLT  1024
-#define STEAM_FLT   2048
-
-// interior power bitwise values
-#define RC2014_ON   1
-#define POWER_ON    2
-#define SUPPORT_ON  4
-#define CIRCUITS_ON 8
-#define SENSORS_ON  16
-#define CONSOLE_ON  32
-#define RC2014_FLT  64
-#define POWER_FLT   128
-#define SUPPORT_FLT 256
-#define CIRCUTS_FLT 512
-#define SENSORS_FLT 1024
-#define CONSOLE_FLT 2048
-
-// general power bitwise values and masks
-#define ANY_FAULT   4096    // bit flagging any part faults
-#define FLT_MASK    0       // TODO: get proper bit mask for checking for faults
-#define FLT_STATE   0       // TODO: get expected state for no faults
-
-#define TT_READY    8192    // bit flagging time travel readiness
-#define TT_MASK     0       // TODO: get proper bit mask for checking time travel state
-#define TT_STATE    0       // TODO: get expected state for time travel readiness
-
-// TODO: determine if there is any need for the top two bits for system checks
+// auxillary status bitwise values
+#define SENSORS_OK  1
+#define SHIELD_OK   2
+#define HOVER_OK    4
+#define TESLA_OK    8
+#define FUSION_OK   16
+#define STEAM_OK    32
 
 /***** data structures *****/
 
@@ -109,12 +97,12 @@ struct time_machine_parts {
 };
 
 // the time machine's power status for all parts
-struct time_machine_status {
-    // could maybe optimize this later with 2 2-bit words
-    uint8_t exterior[6];    // 6 exterior part values
-    uint8_t interior[6];
-    uint8_t computer;       // single switch
-};
+// struct time_machine_status {
+//     // could maybe optimize this later with 2 2-bit words
+//     uint8_t exterior[6];    // 6 exterior part values
+//     uint8_t interior[6];
+//     uint8_t computer;       // single switch
+// };
 
 // the time machine's memory bank
 struct time_machine_ram {
@@ -125,11 +113,15 @@ struct time_machine_ram {
 struct time_machine {
     uint32_t energy;                    // total energy in kJ
     uint8_t tm_status;                  // main status
+
     struct time_machine_parts parts;    // wear & tear
-    struct time_machine_status status;  // display status
-    uint8_t ext_power;                  // bitwise power
-    uint8_t int_power;                  // bitwise power
-    uint8_t computer;                   // power
+    // struct time_machine_status status;  // display status
+
+    uint8_t crit_power;                 // bitwise power
+    uint8_t crit_status;                // bitwise status
+
+    uint8_t aux_power;                  // bitwise power
+    uint8_t aux_status;                 // bitwise status
 };
 
 /***** init functions *****/
@@ -145,11 +137,15 @@ void initialize_tm(uint8_t, struct time_machine*);
 
 /***** bitwise helper functions *****/
 
-// get bitwise power status of an exterior part by ID
-bool get_exterior_power(uint8_t, uint8_t);
+// get bitwise power status of a critical part by ID
+bool get_critical_power(uint8_t, uint8_t);
 
-// get bitwise power status of an interior part by ID
-bool get_interior_power(uint8_t, uint8_t);
+// TODO: add get_critical_status()
+
+// get bitwise power status of an auxillary part by ID
+bool get_auxillary_power(uint8_t, uint8_t);
+
+// TODO: add get_auxillary_status()
 
 // set a bit or bits with the provided mask
 void set_bits(uint8_t* byte, uint8_t mask);
@@ -195,11 +191,11 @@ void turn_off_part(uint8_t, uint8_t, struct time_machine*);
 
 /***** print functions *****/
 
-// returns an exterior part name based on its array ID
-char* get_exterior_part(uint8_t);
+// returns a critical part name based on its array ID
+char* get_critical_part(uint8_t);
 
-// returns an interior part name based on its array ID
-char* get_interior_part(uint8_t);
+// returns an auxillary part name based on its array ID
+char* get_auxillary_part(uint8_t);
 
 // returns a computer part name based on its array ID
 char* get_computer_part(uint8_t);
